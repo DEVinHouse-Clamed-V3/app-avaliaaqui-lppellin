0e979f882383ed05819780c9edd42c0775b972a2
lppellin <thelucaspellin@gmail.com>
2024-10-06 23:24:58 -0300

    navigation initial

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator initialRouteName="Início">
+
+        <Stack.Screen
+          name="Início"
+          component={Inicio}
+          options={{ header: () => <></> }}
+        />
+
+    </NavigationContainer>
   );
 }
 
