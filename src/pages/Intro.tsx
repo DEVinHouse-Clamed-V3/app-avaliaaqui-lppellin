9a1a25c82147777bd3aeef7cf725e642df7a69c5
lppellin <thelucaspellin@gmail.com>
2024-10-07 23:18:36 -0300

    tela intro

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,9 +1,25 @@
-import { Text, View } from "react-native";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import { NavProps } from "../interfaces/NavProps";
 
-export default function Intro() {
+
+
+
+export default function Intro({ navigation }: NavProps) {
+
+
+
+
+
     return (
-        <View>
-            <Text>Intro</Text>
-        </View>
+        <SafeAreaView>
+            <Text>Avalie Aqui</Text>
+
+            <Text>Escolha um produto que deseja avaliar e compartilhe sua experiência com outros consumidores.</Text>
+
+            <TouchableOpacity onPress={() => navigation.navigate("ListaProdutos")}>
+                <Text>Entrar</Text>
+            </TouchableOpacity>
+
+        </SafeAreaView>
     );
 }
