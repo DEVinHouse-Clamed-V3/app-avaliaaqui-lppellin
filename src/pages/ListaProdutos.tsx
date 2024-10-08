b63935b9dd4f43a4db425123501612e04dab7ad3
lppellin <thelucaspellin@gmail.com>
2024-10-07 23:18:51 -0300

    tela lista produtos

--- src/pages/ListaProdutos.tsx
+++ src/pages/ListaProdutos.tsx
@@ -1,10 +1,46 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { FlatList, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import axios from "axios";
+import { NavProps } from "../interfaces/NavProps";
+import { Produto } from "../interfaces/Produto";
 
 
-export default function ListaProdutos() {
+
+
+
+export default function ListaProdutos({ navigation }: NavProps) {
+    const [produtos, setProdutos] = useState<Produto[]>([]);
+
+    useEffect(() => {
+        axios
+            .get(process.env.EXPO_PUBLIC_API_URL + 'products')
+            .then((response) => {
+                setProdutos(response.data);
+            })
+            .catch((error) => {
+                console.error("Erro ao buscar produtos", error);
+            });
+    }, []);
+
     return (
-        <View>
-            <Text>ListaProdutos</Text>
-        </View>
-    )
+        <SafeAreaView>
+            <Text>Lista de Produtos</Text>
+            <FlatList
+                data={produtos}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <View >
+                        <Text>{item.name}</Text>
+
+
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate("AvaliaProduto", { productId: item.id })} // Passando o ID como parâmetro
+                        >
+                            <Text>Avaliar</Text>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
+        </SafeAreaView>
+    );
 }
